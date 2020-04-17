949fcff02891d44e0efdf1dcc24ee8f91c78ebf7
sroro <rodolphe.schnetzer@gmail.com>
2020-04-17 15:29:28 +0200

    Initial Commit

---
+++ Baluchon/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Baluchon
+//
+//  Created by Rodolphe Schnetzer on 17/04/2020.
+//  Copyright © 2020 Rodolphe Schnetzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
